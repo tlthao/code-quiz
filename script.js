a54c84da6e47894b173f2e6dc9b030ff3742454d
Tina Ly <t.tinaly@hotmail.com>
2021-07-06 01:02:16 -0700

    adding javascript variables, editing css for start.html, editing html selectors

---
+++ script.js
@@ -0,0 +1,14 @@
+var next = document.getElementById("next");
+var score = document.getElementById("score");
+var totalScore = document.getElementById("totalScore");
+var countdown = document.getElementById("countdown");
+var count = 0;
+var scoreCount = 0;
+var timer = 60;
+var questions = document.querySelectorAll(".questions");
+var answers = document.querySelectorAll(".questions .answers input");
+
+next.addEventListener('click', function(){
+
+})
+
